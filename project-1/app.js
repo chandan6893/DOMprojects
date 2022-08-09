0a25188ad7f0154d395f317ef30b9792d51ba2a7
Chandan Kumar <krc6893@gmail.com>
2022-08-09 12:59:02 +0530

    Color Flipper demo project

---
+++ project-1/app.js
@@ -0,0 +1,29 @@
+const colors =["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn=document.getElementById("btn");
+const color=document.querySelector(".color");
+btn.addEventListener("click",function(){
+    // const randomNo=Math.floor(Math.random() * colors.length)
+    // getRandomNo();
+    
+    // console.log(randomNo);
+    document.body.style.backgroundColor=colors[Math.floor(Math.random() * colors.length)];
+    color.textContent=colors[Math.floor(Math.random() * colors.length)];
+});
+// function getRandomNo(){
+//     return Math.floor(Math.random() * colors.length);
+// }
+// ************************************
+// btn.addEventListener("click",function(){
+//     for(let i=0;i<colors.length;i++){
+//         color.textContent=colors[i]
+//         console.log(colors[i])
+//         document.body.style.backgroundColor=colors[i];
+//     }
+// });
+        
+        
+
+        
+        
+     
+        
