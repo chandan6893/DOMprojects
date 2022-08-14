288dd43e8b442c8ad2a753f89e7093e531a94023
Chandan Kumar <krc6893@gmail.com>
2022-08-15 01:50:13 +0530

    Reviews Project

--- Interview_DOM_Assign_project/index.js
+++ Interview_DOM_Assign_project/index.js
@@ -1,11 +1,20 @@
 const btns=document.querySelectorAll(".all_btn");
 const create=document.querySelector("c_t_btn");
-// const text=document.querySelector("#textarea");
-const textarea=document.createElement("textarea");
-
-
+const text=document.querySelector("#textarea");
 
 // console.log(btns);
+
+// const textarea=document.createElement("textarea");
+// textarea.classList.add("textarea");
+// textarea.placeholder="Please entertask";
+
+// let formElement=`<form class="form">
+// <textarea name="textarea" class="textarea" cols="30" rows="10" placeholder="please enter task">
+// </textarea> 
+
+// </form>
+// `;
+// textarea.insertAdjacentHTML("beforeend",formElement);
 btns.forEach(function(btn){
     btn.addEventListener("click",function(e){
         const value=e.currentTarget.classList;
@@ -14,3 +23,6 @@ btns.forEach(function(btn){
         }
     })
 })
+
+
+
