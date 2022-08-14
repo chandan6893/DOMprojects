6cf1101a0b45fcecf22bdcd631a8f81723f26f3e
Chandan Kumar <krc6893@gmail.com>
2022-08-14 12:53:26 +0530

    project for interview

--- Interview_DOM_Assign_project/index.js
+++ Interview_DOM_Assign_project/index.js
@@ -1,11 +1,16 @@
-const create=document.getElementById("c_t_btn");
-const form=document.getElementsByClassName("form")
-const textarea_input=getElementById("textarea");
+const btns=document.querySelectorAll(".all_btn");
+const create=document.querySelector("c_t_btn");
+// const text=document.querySelector("#textarea");
+const textarea=document.createElement("textarea");
 
-let input_value=textarea_input.value;
-if(input_value.length>1){
-    
-}
-create.addEventListener("click",function(){
 
-});
+
+// console.log(btns);
+btns.forEach(function(btn){
+    btn.addEventListener("click",function(e){
+        const value=e.currentTarget.classList;
+        if(value.contains("all")){
+        textarea.append(btn)
+        }
+    })
+})
