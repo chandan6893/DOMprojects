ef3115fe3e82f688661ae1f909b376270c174707
Chandan Kumar <krc6893@gmail.com>
2022-08-16 21:40:34 +0530

    Sidebar

--- Navbar/index.js
+++ Navbar/index.js
@@ -6,7 +6,9 @@ toggle.addEventListener("click",function(){
     if(links.classList.contains("links")){
       links.classList.add("show-links")
       links.classList.remove("links")
-    }else if(links.classList.contains("show-links")){
+      
+    }
+    else if(links.classList.contains("show-links")){
 
         links.classList.add("links")
         links.classList.remove("show-links")
