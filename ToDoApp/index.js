7be3b16292b8e1f34b39d537b1f3741393e3f6a4
Chandan Kumar <krc6893@gmail.com>
2022-11-29 00:37:55 +0530

    TODO list

--- ToDoApp/index.js
+++ ToDoApp/index.js
@@ -8,44 +8,61 @@ form.addEventListener("submit",(e)=>{
     e.preventDefault();
     let item = addItemsInput.value;
     // console.log(item)
-    const itemsAndActions=document.createElement("div");
-    itemsAndActions.classList.add("itemsAndActions");
+   if(item.length>1){
+     const itemsAndActions = document.createElement("div");
+     itemsAndActions.classList.add("itemsAndActions");
 
-    const newItems= document.createElement("div");
-    newItems.classList.add("newItems");
+     const newItems = document.createElement("div");
+     newItems.classList.add("newItems");
 
-    itemsAndActions.append(newItems);
+     itemsAndActions.append(newItems);
 
-   const newItemsInput = document.createElement("input");
-   newItemsInput.classList.add("newItemsInput");
-   newItemsInput.type="text";
-   newItemsInput.setAttribute("readonly","readonly")
-//    newItemsInput.value=item; vvi***
-   newItemsInput.value=item;
+     const newItemsInput = document.createElement("input");
+     newItemsInput.classList.add("newItemsInput");
+     newItemsInput.type = "text";
+     //    newItemsInput.setAttribute("readonly","readonly")
+     //    newItemsInput.value=item; vvi***
+     newItemsInput.value = item;
 
-   newItems.append(newItemsInput);
+     newItems.append(newItemsInput);
 
-   const actions= document.createElement("div");
-   actions.classList.add("actions");
+     const actions = document.createElement("div");
+     actions.classList.add("actions");
 
-   itemsAndActions.append(actions);
+     itemsAndActions.append(actions);
 
-   const editSave = document.createElement("button");
-   editSave.classList.add("editSave");
-   editSave.innerText="EDIT"
+     const editSave = document.createElement("button");
+     editSave.classList.add("editSave");
+     editSave.innerText = "SAVE";
 
-   const deleteBtn = document.createElement("button");
-   deleteBtn.classList.add("deleteBtn");
-   deleteBtn.innerText="DELETE";
+     const deleteBtn = document.createElement("button");
+     deleteBtn.classList.add("deleteBtn");
+     deleteBtn.innerText = "DELETE";
 
-   actions.append(editSave);
-   actions.append(deleteBtn);
+     actions.append(editSave);
+     actions.append(deleteBtn);
 
+     items.append(itemsAndActions);
 
-   items.append(itemsAndActions);
+     addItemsInput.value = "";
+     // it would empty the input field
 
-addItemsInput.value="";
-// it would empty the input fied
+     editSave.addEventListener("click", (e) => {
+       if (editSave.innerText == "EDIT") {
+         editSave.innerText = "SAVE";
+         editSave.removeAttribute("readonly");
+       } else if ((editSave.innerText = "SAVE")) {
+         editSave.innerText = "EDIT";
+         editSave.setAttribute("readonly", "readonly");
+       }
+     });
+
+     deleteBtn.addEventListener("click", (e) => {
+       if ((deleteBtn.innerText = "DELETE")) {
+         items.removeChild(itemsAndActions);
+       }
+     });
+   }
 
 
 
